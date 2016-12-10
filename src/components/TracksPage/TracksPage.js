bf8542c5a780888126ea64f1eb8c440873546e7a
ksevezich <kevezich@gmail.com>
2016-12-09 22:15:56 -0500

    added a Procfile.

--- src/components/TracksPage/TracksPage.js
+++ src/components/TracksPage/TracksPage.js
@@ -8,10 +8,10 @@ export const TracksPage = () => (
       <div className={classes.tracks}>
         <h1 className={classes.tracksHeader}>TOPICS</h1>
         <p className={classes.tracksText}>
-          Blueprint: People+Wildlife has four tracks designed to 
-          explore the areas of nature and conservation that directly 
-          affect humans around the world. All of the tracks will have a 
-          leading expert at the conference that will help extend ideas 
+          Blueprint: People+Wildlife has four tracks designed to
+          explore the areas of nature and conservation that directly
+          affect humans around the world. All of the tracks will have a
+          leading expert at the conference that will help extend ideas
           beyond the event.
         </p>
         <TracksGrid />
