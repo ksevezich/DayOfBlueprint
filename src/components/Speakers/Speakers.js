199df3cd8c92584e6e752de87fbb68098d15409a
ksevezich <kevezich@gmail.com>
2016-12-11 12:32:00 -0500

    Compresses image
    Compresses the background image for faster load

--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -22,10 +22,10 @@ export const Speakers = () => (
           text={'Dr. Stephen T. Lee is the Chief Scientist (ST) at the U.S. \
           Army Research Office (ARO). He is interested in how technology can be \
           used to combat illegal poaching and the wildlife trade in Southern Africa.\
-          "Dr. Lee makes the case that reducing the illegal wildlife trade and poaching in \
+          Dr. Lee makes the case that reducing the illegal wildlife trade and poaching in \
           Africa is in the best interest of the international community regarding safety, \
           security, conservation and goodwill. Further, that science and technology have \
-          pivotal roles to play via next generation tools and capabilities."'}
+          pivotal roles to play via next generation tools and capabilities.'}
           imagePath={stephen} />
         <SpeakersSection title={'Stay Tuned'}
           beforeText={'We will be adding speakers as we get closer to the event. Want to be a speaker at this event? '}
