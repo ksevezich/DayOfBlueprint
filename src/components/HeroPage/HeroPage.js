b29f580c7033ad6c4ccd59772f0369a5cc6dccf1
ksevezich <kevezich@gmail.com>
2016-12-12 22:09:04 -0500

    favicon fix? and removes date from Hero
    favicon fix? and removes date from Hero

--- src/components/HeroPage/HeroPage.js
+++ src/components/HeroPage/HeroPage.js
@@ -6,11 +6,11 @@ export const HeroPage = () => (
     <div className={classes.hero}>
       <div className={classes.mainText}>
         <img src='blueprint-logo.png' alt='Blueprint Logo' />
-        <h6>January 27-28, 2017</h6>
+        <h6> </h6>
         <div className={classes.buttonDiv}>
           <a className={classes.applyLink}
             href={'https://dukeblueprint.typeform.com/to/fhXF3I?route_receive_participant=xxxxx'}>
-            <button className={classes.homeButton}> Apply Now </button>
+            <button className={classes.homeButton}> Register Now </button>
           </a>
         </div>
       </div>
