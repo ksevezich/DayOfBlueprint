28b3706dba892e60eaf73ca6cbff1c0e4c30d28c
ksevezich <kevezich@gmail.com>
2017-01-27 18:52:14 -0500

    condenses speakers
    condenses speakers

--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -11,26 +11,26 @@ export const Speakers = () => (
     <div className={classes.speakers} >
       <div className={'container text-center'}>
         <h1 className={classes.header}> Speakers </h1>
-        <SpeakersSection title={'Alex Dehgan, Conservation X Labs, CEO and Founder'}
-          beforeText={'Alex Dehgan will talk about Invasive Species. Contact him at '}
+        <SpeakersSection title={'Alex Dehgan, Conservation X Labs'}
+          beforeText={'Alex Dehgan, CEO and founder of Conservation X Labs, will talk about Invasive Species. Contact him at '}
           link={'mailto:alexdehgan@gmail.com'}
           linkText={'here'}
-          imagePath={alex} />
-        <SpeakersSection title={'Stephen Lee, U.S. Army Reserach Office, Chief Scientist'}
-          beforeText={'Dr. Stephen T. Lee will discuss Wildlife Trafficking. Contact him at '}
-          link={'mailto:stephen.j.lee28.civ@mail.mil'}
-          linkText={'here'}
-          imagePath={stephen} />
-        <SpeakersSection title={'Jason Heckathorn, Forever Oceans, CEO'}
-          beforeText={'Jason Heckathorn is our Keynote speaker. Contact him at '}
+          imagePath={alex}
+          titleSecond={'Stephen Lee, U.S. Army Research Office'}
+          beforeTextSecond={'Dr. Stephen T. Lee, Chief Scientist at the U.S. Army Research Office will discuss Wildlife Trafficking. Contact him at '}
+          linkSecond={'mailto:stephen.j.lee28.civ@mail.mil'}
+          linkTextSecond={'here'}
+          imagePathSecond={stephen} />
+        <SpeakersSection title={'Jason Heckathorn, Forever Oceans'}
+          beforeText={'Jason Heckathorn, CEO of Forever Oceans, is our Keynote speaker. Contact him at '}
           link={'mailto:jason.heckathorn@foreveroceans.com'}
           linkText={'here'}
-          imagePath={jason} />
-        <SpeakersSection title={'Dan Fay, Microsoft Research, Director of External Research for Earth, Energy, and Environment'}
-          beforeText={'Daniel Fay will discuss Citizen Science. Contact him at '}
-          link={'mailto:dan.fay@microsoft.com'}
-          linkText={'here'}
-          imagePath={dan} />
+          imagePath={jason}
+          titleSecond={'Dan Fay, Microsoft Research'}
+          beforeTextSecond={'Daniel Fay, the Director of External Research for Earth, Energy, and Environment at Microsoft Research, will discuss Citizen Science. Contact him at '}
+          linkSecond={'mailto:dan.fay@microsoft.com'}
+          linkTextSecond={'here'}
+          imagePathSecond={dan} />
       </div>
     </div>
   </div>
--- src/components/SpeakersSection/SpeakersSection.js
+++ src/components/SpeakersSection/SpeakersSection.js
@@ -6,20 +6,37 @@ export const SpeakersSection = (props) => (
 
   <div>
     <Row className={classes.section}>
-      <Col md={2}>
+      <Col md={1}>
         <div>
           <img className={classes.sectionImage} src={props.imagePath} />
         </div>
       </Col>
-      <Col md={4}>
+      <Col md={2}>
         <div className={classes.sectionHeader}>
           {props.title}
         </div>
       </Col>
-      <Col md={6}>
+      <Col md={3}>
         <div className={classes.sectionText}>
           {(props.link) ? (
             generateTextWithMultiLinks(props.beforeText, props.link, props.linkText)
+        ) : props.text }
+        </div>
+      </Col>
+      <Col md={1}>
+        <div>
+          <img className={classes.sectionImage} src={props.imagePathSecond} />
+        </div>
+      </Col>
+      <Col md={2}>
+        <div className={classes.sectionHeader}>
+          {props.titleSecond}
+        </div>
+      </Col>
+      <Col md={3}>
+        <div className={classes.sectionText}>
+          {(props.linkSecond) ? (
+            generateTextWithMultiLinks(props.beforeTextSecond, props.linkSecond, props.linkTextSecond)
         ) : props.text }
         </div>
       </Col>
@@ -46,7 +63,17 @@ SpeakersSection.propTypes = {
   linkText: React.PropTypes.string,
   linkText0: React.PropTypes.string,
   link: React.PropTypes.string,
-  link0: React.PropTypes.string
+  link0: React.PropTypes.string,
+  titleSecond: React.PropTypes.string.isRequired,
+  textSecond: React.PropTypes.string.isRequired,
+  imagePathSecond: React.PropTypes.string.isRequired,
+  beforeTextSecond: React.PropTypes.string,
+  afterTextSecond: React.PropTypes.string,
+  middleTextSecond: React.PropTypes.string,
+  linkTextSecond: React.PropTypes.string,
+  linkText0Second: React.PropTypes.string,
+  linkSecond: React.PropTypes.string,
+  link0Second: React.PropTypes.string
 }
 
 export default SpeakersSection
