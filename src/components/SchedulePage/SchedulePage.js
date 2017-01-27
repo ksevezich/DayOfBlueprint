e1965839a22a5347be70ccb31b45f44bab076c24
ksevezich <kevezich@gmail.com>
2017-01-27 11:25:32 -0500

    fixes schedule
    fixes schedule

--- src/components/SchedulePage/SchedulePage.js
+++ src/components/SchedulePage/SchedulePage.js
@@ -3,7 +3,10 @@ import classes from './SchedulePage.scss'
 
 export const SchedulePage = () => (
   <div>
-    <div className={classes.hero}>
+    <div className={classes.schedule}>
+      <div className={classes.mainText}>
+        <img src='calendar.png' alt='Blueprint Calendar' />
+      </div>
     </div>
   </div>
 )
